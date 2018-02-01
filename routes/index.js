dbd3ffc573f5a2535311f0cf92c70681363e6386
Troy Lamerton <troy.lamerton@gmail.com>
2018-02-01 13:18:05 +0100

    add form page that send public address to server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* simpel page with input
+can put in my address and send request*/
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/find', function(req, res) {
+  const publicAddress = req.body.address;
+
+  /*
+  request triggers using web3 to go through finding all possible hodl ids
+  and logs them to console
+  */
+
+  res.status(200).send();
 });
 
 module.exports = router;
